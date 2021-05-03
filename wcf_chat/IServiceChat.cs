7f9b6ec4afcc24150f877b7be06f57e9482221a6
Griland <grishabulgak@gmail.com>
2021-05-03 23:27:30 +0300

    orderscreation

--- wcf_chat/IServiceChat.cs
+++ wcf_chat/IServiceChat.cs
@@ -18,15 +18,31 @@ namespace wcf_chat
         [OperationContract]
         void Disconnect();
 
-        [OperationContract(IsOneWay = true)]
-        void SendMsg(string login);
+        [OperationContract]
+        string SendDB(string login);
         [OperationContract]
         bool TryLogin(string login, string password);
+        [OperationContract]
+        bool TryRegister(string login, string password, string name, string surname);
+        [OperationContract]
+        string SendMarks();
+        [OperationContract]
+        string SendModels(string mark);
+        [OperationContract]
+        bool TryOrder(int transport, string number, int creator, string creationdate, string endingdate);
+        [OperationContract]
+        int GetTransport(string mark, string model);
+        [OperationContract]
+        int GetUserID(string login);
     }
 
     public interface IServerChatCallback
     {
         [OperationContract(IsOneWay = true)]
         void MsgCallback(string msg);
+        [OperationContract(IsOneWay = true)]
+        void MarksCallback(string msg);
+        [OperationContract(IsOneWay = true)]
+        void ModelsCallback(string msg);
     }
 }
--- wcf_chat/ServiceChat.cs
+++ wcf_chat/ServiceChat.cs
@@ -14,7 +14,8 @@ namespace wcf_chat
     public class ServiceChat : IServiceChat
     {
         List<OrderInfo> orderInfos = new List<OrderInfo>();
-        List<ServerUser> users = new List<ServerUser>();
+        List<string> marks = new List<string>();
+        List<string> models = new List<string>();
         ServerUser user;
         public void Connect()
         {
@@ -31,12 +32,13 @@ namespace wcf_chat
             user = null;
         }
 
-        public void SendMsg(string login)
+        public string SendDB(string login)
         {
             if (IsAdmin(login))
             {
                 using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
                 {
+                    orderInfos.Clear();
                     connection.Open();
                     string select = "select * from OrderInfo";
                     SqlCommand cmd = new SqlCommand(select, connection);
@@ -50,16 +52,17 @@ namespace wcf_chat
                         orderInfo.OrderInfo_CreationDate = dr[4].ToString();
                         orderInfo.OrderInfo_EndingDate = dr[5].ToString();
                         orderInfos.Add(orderInfo);
-                        string json = JsonConvert.SerializeObject(orderInfos);
-                        user.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(json);
+                        //string json = JsonConvert.SerializeObject(orderInfos);
+                        //user.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(json);
                     }
-                    orderInfos.Clear();
+                    return JsonConvert.SerializeObject(orderInfos);
                 }
             }
             else
             {
                 using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
                 {
+                    orderInfos.Clear();
                     connection.Open();
                     string select = "select * from OrderInfo o join UserInfo u on o.OrderInfo_Creator=u.UserInfo_ID where u.UserInfo_Login=@login";
                     SqlCommand cmd = new SqlCommand(select, connection);
@@ -75,10 +78,10 @@ namespace wcf_chat
                         orderInfo.OrderInfo_CreationDate = dr[4].ToString();
                         orderInfo.OrderInfo_EndingDate = dr[5].ToString();
                         orderInfos.Add(orderInfo);
-                        string json = JsonConvert.SerializeObject(orderInfos);
-                        user.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(json);
+                        //string json = JsonConvert.SerializeObject(orderInfos);
+                        //user.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(json);
                     }
-                    orderInfos.Clear();
+                    return JsonConvert.SerializeObject(orderInfos);
                 }
             }
         }
@@ -120,14 +123,177 @@ namespace wcf_chat
                 SqlDataReader dr = cmd.ExecuteReader();
                 if (dr.HasRows)
                 {
-                    Console.WriteLine("Login True");
+                    if(IsAdmin(login))
+                        Console.WriteLine("Logged as admin");
+                    else
+                        Console.WriteLine("Logged as user");
                     return true;
                 }
                 else
                 {
-                    Console.WriteLine("Login False");
+                    Console.WriteLine("Login failed");
                     return false;
                 }
+            }
+        }
+
+        public bool TryRegister(string login, string password, string name, string surname)
+        {
+            if (!SearchLogin(login))
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+                    {
+                        connection.Open();
+                        string insert = "insert into UserInfo values (@login,@password,@name,@surname,'false')";
+                        SqlCommand cmd = new SqlCommand(insert, connection);
+                        SqlParameter loginParam = new SqlParameter("@login", login);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@password", password);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@name", name);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@surname", surname);
+                        cmd.Parameters.Add(loginParam);
+                        SqlDataReader dr = cmd.ExecuteReader();
+                        if (SearchLogin(login))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+        public bool SearchLogin(string login)
+        {
+            using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+            {
+                connection.Open();
+                string select = "select * from UserInfo where UserInfo_Login = @login";
+                SqlCommand cmd = new SqlCommand(select, connection);
+                SqlParameter loginParam = new SqlParameter("@login", login);
+                cmd.Parameters.Add(loginParam);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public string SendMarks()
+        {
+            using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+            {
+                marks.Clear();
+                connection.Open();
+                string select = "select TransportInfo_Mark from TransportInfo group by TransportInfo_Mark order by TransportInfo_Mark";
+                SqlCommand cmd = new SqlCommand(select, connection);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    marks.Add(dr[0].ToString());
+                    //string json = JsonConvert.SerializeObject(marks);
+                    //user.operationContext.GetCallbackChannel<IServerChatCallback>().MarksCallback(json);
+                }
+                return JsonConvert.SerializeObject(marks);
+            }
+        }
+        public string SendModels(string mark)
+        {
+            using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+            {
+                models.Clear();
+                connection.Open();
+                string select = "select TransportInfo_Model from TransportInfo where TransportInfo_Mark = @mark";
+                SqlCommand cmd = new SqlCommand(select, connection);
+                SqlParameter Param = new SqlParameter("@mark", mark);
+                cmd.Parameters.Add(Param);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    models.Add(dr[0].ToString());
+                    //string json = JsonConvert.SerializeObject(models);
+                    //user.operationContext.GetCallbackChannel<IServerChatCallback>().ModelsCallback(json);
+                }
+                return JsonConvert.SerializeObject(models);
+            }
+        }
+
+        public bool TryOrder(int transport, string number, int creator, string creationdate, string endingdate)
+        {
+
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+                    {
+                        connection.Open();
+                        string insert = "insert into OrderInfo values(@transport,@number,@creator,convert(datetime,@creationdate,20),convert(datetime,@endingdate,20))";
+                        SqlCommand cmd = new SqlCommand(insert, connection);
+                        SqlParameter loginParam = new SqlParameter("@transport", transport);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@number", number);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@creator", creator);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@creationdate", creationdate);
+                        cmd.Parameters.Add(loginParam);
+                        loginParam = new SqlParameter("@endingdate", endingdate);
+                        cmd.Parameters.Add(loginParam);
+                        SqlDataReader dr = cmd.ExecuteReader();
+                        return true;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+
+        }
+
+        public int GetTransport(string mark, string model)
+        {
+            using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+            {
+                connection.Open();
+                string select = "select TransportInfo_ID from TransportInfo where TransportInfo_Mark = @mark and TransportInfo_Model = @model";
+                SqlCommand cmd = new SqlCommand(select, connection);
+                SqlParameter Param = new SqlParameter("@mark", mark);
+                cmd.Parameters.Add(Param);
+                Param = new SqlParameter("@model", model);
+                cmd.Parameters.Add(Param);
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                return Convert.ToInt32(dr[0]);
+            }
+        }
+        public int GetUserID(string login)
+        {
+            using (SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BGV_CP;Data Source=MRGRILAND"))
+            {
+                connection.Open();
+                string select = "select UserInfo_ID from UserInfo where UserInfo_Login = @login";
+                SqlCommand cmd = new SqlCommand(select, connection);
+                SqlParameter loginParam = new SqlParameter("@login", login);
+                cmd.Parameters.Add(loginParam);
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                return Convert.ToInt32(dr[0]);
             }
         }
     }
